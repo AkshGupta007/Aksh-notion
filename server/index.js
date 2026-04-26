0fcc854c3a8be9d21a65e308c7c31661bcdd16b5
aksh <akshgupta593@gmail.com>
2026-04-26 18:36:02 +0530

    changes made some

--- server/index.js
+++ server/index.js
@@ -50,6 +50,6 @@ cloudinaryconfig();
 app.get('/',(req,res)=>{
     res.send("welcome to study notion");
 });
-app.listen(4000,()=>{
-    console.log("server is running on port 4000");
+app.listen(process.env.PORT || 4000,()=>{
+    console.log("server is running on port " + (process.env.PORT || 4000));
 })
